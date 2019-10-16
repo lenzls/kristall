3a2ca777a5e69a1c0bfaaed4c8e58363f785be0c
Simon Lenz <dev@simon-lenz.de>
2019-10-16 23:49:18 +0200

    added first person view

--- Assets/Scripts/playerCollision.cs
+++ Assets/Scripts/playerCollision.cs
@@ -2,31 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class playerCollision : MonoBehaviour
 {
     
     private Vector3 initialPosition;
+    private Quaternion initialRotation;
     public GameObject tryAgainText;
 
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = transform.position;
+        initialRotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("monster")) {
             // Destroy(other.gameObject);
+            Debug.Log("collission with monster!########################################################################################################");
+
             tryAgainText.GetComponent<TextFadeability>().Fade();
+
             transform.position = initialPosition;
+            GetComponent<FirstPersonController>().m_MouseLook.ResetCharacterTargetRot(initialRotation);
             GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
         }
     }
