63b944afc0f8f9a82d21742a6670f42e50d83450
Simon Lenz <dev@simon-lenz.de>
2019-10-21 00:16:10 +0200

    add new improved cameraswitch

---
+++ Assets/Scripts/NewCameraSwitcher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCameraSwitcher : MonoBehaviour
+{
+    public GameObject firstPersonCamera;
+    public GameObject thirdPersonCamera;
+    public GameObject aboveCamera;
+
+    public int cameraMode = 0;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Camera"))
+        {
+            if (cameraMode == 2) 
+            {
+                cameraMode = 0;
+            }
+            else 
+            {
+                cameraMode += 1;
+            }
+        }
+        StartCoroutine(CameraChange());
+    }
+
+    IEnumerator CameraChange() 
+    {
+        yield return new WaitForSeconds(0.01f);
+        if (cameraMode == 0)
+        {
+            firstPersonCamera.SetActive (true);
+            thirdPersonCamera.SetActive (false);
+            aboveCamera.SetActive (false);
+        }
+        if (cameraMode == 1)
+        {
+            firstPersonCamera.SetActive (false);
+            thirdPersonCamera.SetActive (true);
+            aboveCamera.SetActive (false);
+        }
+        if (cameraMode == 2)
+        {
+            firstPersonCamera.SetActive (false);
+            thirdPersonCamera.SetActive (false);
+            aboveCamera.SetActive (true);
+        }
+    }
+}
